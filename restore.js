8fc386b15fcffe6aab28357a2e5a5be937836bc3
unpete <info@oknosoft.ru>
2018-06-17 23:02:10 +0500

    backup - restore

--- restore.js
+++ restore.js
@@ -21,9 +21,11 @@ const debug = require('debug')('wb:restore');
 const PouchDB = require('./pouchdb');
 const path = require('path');
 const fs = require('fs');
+const JSZip = require('jszip');
 
 debug('required');
-
+const mb = 1024 * 1024;
+let docs = 0;
 
 const yargs = require('yargs')
   .demand(1)
@@ -53,59 +55,7 @@ const yargs = require('yargs')
         dst.info()
           .then((info) => {
             debug(`connected to ${info.host}, doc count: ${info.doc_count}`);
-
-            // в tail (хвост) будем складывать неполные объекты
-            let tail = '';
-            let docs = 0;
-
-            // открываем файловый поток
-            const mb = 1024 * 1024;
-            const stream = fs.createReadStream(from, {highWaterMark: mb * 4});
-            stream.on('data', (chunk) => {
-              // тормозим поток
-              stream.pause();
-              // делаем синтаксический разбор
-              const data = chunk.toString().replace(/,\n/g, '\n').split('\n');
-              if(data[0] === '{"new_edits":false,"docs":[') {
-                data.splice(0, 1);
-              }
-              if(tail) {
-                data[0] = tail + data[0];
-                tail = '';
-              }
-              const last = data[data.length - 1];
-              try {
-                if(last === ']}') {
-                  data.splice(data.length - 1, 1);
-                  debug('all done');
-                  process.exit(0);
-                }
-                else {
-                  const test = JSON.parse(last)
-                }
-              }
-              catch(err) {
-                tail = last;
-                data.splice(data.length - 1, 1);
-              }
-              // отправляем данные в couchdb
-              try {
-                dst.bulkDocs(data.map((str) => JSON.parse(str)), {new_edits: false})
-                  .then((rows) => {
-                    docs += data.length;
-                    process.stdout.write(`\u001b[2K\u001b[0E\t${stream.bytesRead / mb}Mb read, ${docs} docs written`);
-                    stream.resume();
-                  })
-                  .catch((err) => {
-                    debug(err);
-                    process.exit(1);
-                  });
-              }
-              catch(err) {
-                debug(err);
-                process.exit(1);
-              }
-            });
+            restore(from, dst);
           })
           .catch(err => {
             debug(err);
@@ -119,6 +69,138 @@ const yargs = require('yargs')
       }
     })
   .epilog('\nMore information about the library: https://github.com/oknosoft/windowbuilder');
+
+function restore(from, dst) {
+  // если from указывает на каталог, перебираем файлы архивов
+  try{
+    if(fs.lstatSync(from).isDirectory()) {
+      const files = fs.readdirSync(from).filter((file) => {
+        const stat = fs.statSync(path.join(from, file));
+        return stat.isFile() && file.indexOf('.zip') !== -1;
+      });
+      fromFiles(from, files, dst);
+    }
+    else {
+      // открываем файловый поток
+      const stream = fs.createReadStream(from, {highWaterMark: mb * 4});
+      fromFile(stream, dst)
+        .then(() => {
+          debug('\nall done');
+        })
+        .catch(err => {
+          debug(err);
+          process.exit(1);
+        });
+    }
+  }catch(e){
+    debug(err);
+    process.exit(1);
+  }
+}
+
+function fromFiles(from, files, dst) {
+  return new Promise((resolve, reject) => {
+    if(!files.length) {
+      return resolve();
+    }
+    const file = files.splice(0, 1)[0];
+    // read a zip file
+    fs.readFile(path.join(from, file), (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      JSZip.loadAsync(data).then((zip) => {
+        const content = zip.file(file.replace('.zip', '.json'));
+        const stream = content.nodeStream();
+        fromFile(stream, dst)
+          .then(() => {
+            resolve();
+          })
+      });
+    });
+  })
+    .then(() => {
+      if(files.length) {
+        return fromFiles(from, files, dst);
+      }
+    });
+}
+
+function fromFile(stream, dst) {
+
+  return new Promise((resolve, reject) => {
+    // в tail (хвост) будем складывать неполные объекты
+    let tail = '';
+    let substr = '';
+    let finish;
+
+    stream.on('data', (chunk) => {
+
+      substr += chunk.toString();
+      if(!finish && substr.length < mb * 4) {
+        return;
+      }
+
+      // тормозим поток
+      stream.pause();
+      // делаем синтаксический разбор
+      const data = substr.replace(/,\n/g, '\n').split('\n');
+      substr = '';
+      if(data[0] === '{"new_edits":false,"docs":[' || data[0] === '') {
+        data.splice(0, 1);
+      }
+      if(tail) {
+        data[0] = tail + data[0];
+        tail = '';
+      }
+      let last = data[data.length - 1];
+      try {
+        while(last === '') {
+          data.splice(data.length - 1, 1);
+          last = data[data.length - 1];
+          finish = true;
+        }
+        if(last === ']}') {
+          data.splice(data.length - 1, 1);
+          finish = true;
+        }
+        else {
+          const test = JSON.parse(last);
+        }
+      }
+      catch(err) {
+        tail = last;
+        data.splice(data.length - 1, 1);
+      }
+      // отправляем данные в couchdb
+      try {
+        data.length && dst.bulkDocs(data.map((str) => JSON.parse(str)), {new_edits: false})
+          .then((rows) => {
+            docs += data.length;
+            process.stdout.write(`\u001b[2K\u001b[0E\t${stream.bytesRead / mb}Mb read, ${docs} docs written`);
+            if(finish) {
+              resolve();
+            }
+            else {
+              stream.resume();
+            }
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      }
+      catch(err) {
+        reject(err);
+      }
+    })
+      .on('end', () => {
+        finish = true;
+        if(substr) {
+          stream.emit('data', new Buffer([]));
+        }
+      });
+  });
+}
 
 const {argv} = yargs;
 if(!argv._.length){
