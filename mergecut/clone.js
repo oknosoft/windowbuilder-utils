37afeb5258a3babdbd8d12904510622b76a1c051
unpete <info@oknosoft.ru>
2020-01-17 14:10:34 +0500

    групповой clone local_docs (для couchdb 2.2 и выше)

--- mergecut/clone.js
+++ mergecut/clone.js
@@ -17,7 +17,7 @@ const cnames = ['doc.calc_order', 'cat.characteristics'];
 const progress = require('./progress.json');
 let step = 0;
 
-module.exports = function ({src, tgt, suffix, all_docs, all_dbs, local_docs, exclude = [], include = [], remove = []}) {
+module.exports = function ({src, tgt, suffix, all_docs, all_dbs, local_docs, exclude = [], include = [], remove = [], skip_security}) {
 
   let index = 0;
 
@@ -29,7 +29,7 @@ module.exports = function ({src, tgt, suffix, all_docs, all_dbs, local_docs, exc
       return next(dbs);
     }
     if(name && (all_dbs || name[0] !== '_') && !exclude.includes(name)) {
-      return clone({src, tgt, name, suffix, all_docs, local_docs, remove})
+      return clone({src, tgt, name, suffix, all_docs, local_docs, remove, skip_security})
         .then(() => next(dbs));
     }
     if(name) {
@@ -57,7 +57,7 @@ function sleep(time, res) {
 }
 
 // выполняет обслуживание
-function clone({src, tgt, name, suffix, all_docs, local_docs, remove}) {
+function clone({src, tgt, name, suffix, all_docs, local_docs, remove, skip_security}) {
   // получаем базы
   src = new PouchDB(`${src}/${name}`, {
     auth: {
@@ -76,7 +76,7 @@ function clone({src, tgt, name, suffix, all_docs, local_docs, remove}) {
     ajax: {timeout}
   });
 
-  return clone_security(src, tgt)
+  return (skip_security ? Promise.resolve() : clone_security(src, tgt))
     .then(() => clone_local_docs(src, tgt, local_docs))
     .then(() => next_docs(src, tgt, progress[src.name] || '', all_docs))
     .then(() => {
@@ -152,11 +152,28 @@ function clone_local_docs(src, tgt, local_docs) {
   })
     .then((res) => res.json())
     .then(async ({rows}) => {
+
+      // for(const row of rows) {
+      //   if(row.id.startsWith('_local/bar')) {
+      //     await clone_local_doc(src, tgt, row.id);
+      //   }
+      // };
+
+      const keys = [];
       for(const row of rows) {
-        if(row.id.includes('log.') || row.id.includes('store.')) {
-          await clone_local_doc(src, tgt, row.id);
+        if(row.id.startsWith('_local/bar')) {
+          keys.push(row.id);
+        }
+        if(keys.length > 200) {
+          await src.allDocs({include_docs: true, keys})
+            .then(({rows}) => clone_docs(rows, tgt, true));
+          keys.length = 0;
         }
       };
+      if(keys.length) {
+        await src.allDocs({include_docs: true, keys})
+          .then(({rows}) => clone_docs(rows, tgt, true));
+      }
     })
 }
 
--- mergecut/config.js
+++ mergecut/config.js
@@ -8,40 +8,41 @@
 module.exports = {
   start: '2019-09-30',
   tasks: {
-    kaleva: {
-      test: /_8_doc/,
-      dbs: [],
-      // эти серверы будут клонированы с обрезанием, массив может быть пустым
-      clone: [
-        // {
-        //   src: 'http://192.168.21.208:5984',
-        //   tgt: 'http://192.168.21.109:5984',
-        // },
-      ],
-      // данные этих баз будут объединены в tgt, массив может быть пустым
-      merge: [
-        {
-          exclude: ['wb_8_doc'],
-          // можно указать список серверов
-          src: ['https://c208.oknosoft.com'],
-          tgt: 'https://c209.oknosoft.com/wb_8_doc'
-        },
-      ]
-    },
+    // kaleva: {
+    //   test: /_8_doc/,
+    //   dbs: [],
+    //   // эти серверы будут клонированы с обрезанием, массив может быть пустым
+    //   clone: [
+    //     // {
+    //     //   src: 'http://192.168.21.208:5984',
+    //     //   tgt: 'http://192.168.21.109:5984',
+    //     // },
+    //   ],
+    //   // данные этих баз будут объединены в tgt, массив может быть пустым
+    //   merge: [
+    //     {
+    //       exclude: ['wb_8_doc'],
+    //       // можно указать список серверов
+    //       src: ['https://c208.oknosoft.com'],
+    //       tgt: 'https://c209.oknosoft.com/wb_8_doc'
+    //     },
+    //   ]
+    // },
     crystallit: {
-      test: /_25_doc/,
+      test: /_21_doc/,
       dbs: [],
       clone: [
         {
-          src: 'https://crystallit.oknosoft.ru/couchdb',
-          tgt: 'https://crystallit.oknosoft.ru/c2020',
+          tgt: 'http://oknosoft.ecookna.ru:5984',
+          src: 'https://dh5.oknosoft.ru:221',
           // tgt: 'http://cou221:5984',
-          // suffix: '2019',
+          suffix: '0100',
           // all_docs: true,
           // all_dbs: true,
+          skip_security: true,
           local_docs: true,
           exclude: [],
-          include: [],
+          include: ['wb_21_doc'],
           remove: []
         },
       ],
